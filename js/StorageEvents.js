09b91628cae01b7a332678cb4d476432196c8c47
Commander-lol <ljcapitanio@gmail.com>
2014-12-28 17:59:24 +0000

    Initial Commit, version 1.0 complete set of basic features

---
+++ js/StorageEvents.js
@@ -0,0 +1,70 @@
+/*global CustomEvent */
+/*jslint plusplus: true */
+/*jslint nomen: true */
+(function () {
+    'use strict';
+    var StorageEvent = function (info) {
+            var ev = new CustomEvent("storage", {bubbles: true, cancelable: false});
+            ev.key = info.key;
+            ev.newValue = info.newValue;
+            ev.oldValue = info.oldValue;
+            return ev;
+        },
+        Storage = function () {
+            var _this = this;
+            this.storagemethod = localStorage;
+            this.setMethod = function (newMethod) {
+                _this.storagemethod = newMethod;
+            };
+            this.setItem = function (key, value) {
+                var oldValue = _this.storagemethod.getItem(key),
+                    event = new StorageEvent({
+                        key: key,
+                        newValue: value,
+                        oldValue: oldValue
+                    });
+                _this.storagemethod.setItem(key, value);
+                window.dispatchEvent(event);
+            };
+            this.getItem = function (key) {
+                return _this.storagemethod.getItem(key);
+            };
+            this.removeItem = function (key) {
+                var oldValue = _this.storagemethod.getItem(key),
+                    pEvent,
+                    exists = (typeof oldValue !== "undefined") && (oldValue !== null);
+                _this.storagemethod.removeItem(key);
+                if (exists) {
+                    pEvent = new StorageEvent({
+                        key: key,
+                        oldValue: oldValue,
+                        newValue: null
+                    });
+                    window.dispatchEvent(pEvent);
+                }
+            };
+            this.clear = function () {
+                var events = [],
+                    i,
+                    key,
+                    curData;
+                for (i = 0; i < _this.storagemethod.length; i++) {
+                    key = _this.storagemethod.key(i);
+                    curData = _this.storagemethod.getItem(key);
+                    events.push(new StorageEvent({
+                        key: key,
+                        oldValue: curData,
+                        newValue: null
+                    }));
+                }
+                _this.storagemethod.clear();
+                for (i = 0; i < _this.events.length; i++) {
+                    window.dispatchEvent(events[i]);
+                }
+            };
+        };
+
+    if (!window.storage) {
+        window.storage = new Storage();
+    }
+}());
